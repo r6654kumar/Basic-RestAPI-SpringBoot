a46fae36ece53757c467db56dda3342fe02aeeae
Rahul <rsc120@nist.edu>
2024-11-09 22:13:57 +0530

    Project Initialized

---
+++ src/main/java/com/basic_restapi/Basic/RestAPI/SpringBoot/BasicRestApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.basic_restapi.Basic.RestAPI.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicRestApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicRestApiSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basic_restapi/Basic/RestAPI/SpringBoot/BasicRestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic_restapi.Basic.RestAPI.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRestApiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
